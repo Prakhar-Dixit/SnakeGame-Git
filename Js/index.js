5e0d5f1d73ddc0b02cccb3a8402aaa095cd9a158
Prakhar Dixit <75660779+Prakhar-Dixit@users.noreply.github.com>
2021-12-01 12:10:50 +0530

    Update index.js

--- Js/index.js
+++ Js/index.js
@@ -4,7 +4,7 @@ const foodSound = new Audio('food.mp3');
 const gameOverSound = new Audio('gameover.mp3');
 const moveSound = new Audio('move.mp3');
 const musicSound = new Audio('music.mp3');
-let speed = 10;
+let speed = 4;
 let score = 0;
 let lastPaintTime = 0;
 let snakeArr = [
@@ -25,7 +25,7 @@ function main(ctime) {
 }
 
 function isCollide(snake) {
-    // If you bump into yourself 
+    // If you bump into yourself
     for (let i = 1; i < snakeArr.length; i++) {
         if(snake[i].x === snake[0].x && snake[i].y === snake[0].y){
             return true;
@@ -42,6 +42,7 @@ function isCollide(snake) {
 function gameEngine() {
     // part-1: Updating the snake array and food 
     if (isCollide(snakeArr)) {
+        speed = 4;
         gameOverSound.play();
         musicSound.pause();
         inputDir = { x: 0, y: 0 };
@@ -55,6 +56,7 @@ function gameEngine() {
     if (snakeArr[0].y === food.y && snakeArr[0].x === food.x) {
         foodSound.play();
         score += 1;
+        speed++;
         if(score>hiscoreval){
             hiscoreval = score;
             localStorage.setItem("hiscore",JSON.stringify(hiscoreval));
@@ -104,7 +106,6 @@ function gameEngine() {
 }
 
 // Main Logic Starts Here
-musicSound.play();
 let hiscore = localStorage.getItem("hiscore");
 if(hiscore === null){
     hiscoreval=0;
@@ -118,6 +119,7 @@ window.requestAnimationFrame(main);
 window.addEventListener('keydown', e => {
     inputDir = { x: 0, y: 1 };
     moveSound.play();
+    musicSound.play();
     switch (e.key) {
         case "ArrowUp":
             console.log("ArrowUp")
